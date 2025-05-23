760bc2583fee3dd8d86c2f30a41eb56e61e16fe1
vhhonorato <vhhonorato@hotmail.com>
2025-05-23 16:50:23 -0300

    Criada função para buscar usuário pelo nome./Feito testes e ajustes no código.

--- case01.py
+++ case01.py
@@ -2,10 +2,11 @@
 
 def menu():
     #Função principal para gerenciar aplicação
+   
     usuarios = []
 
     while True:
-        print("Menu Principal")
+        print("\nMenu Principal")
         print("1. Cadastrar usuário")
         print("2. Listar usuário")
         print("3. Buscar usuário por nome")
@@ -30,9 +31,9 @@ def cadastrar_usuario(usuarios):
     #Função para cadastrar novo usuário. 
     # Seus dados(nome, email e idade) serão inseridos na variavel 'usuarios', dentro da função 'menu', já que a variável 'usuario' foi passada como argumento da função 'cadastrar_usuario'
 
-    print("Cadastro de Usuário")
-    nome = input("Insira o nome do usuário")
-    email = input("Insira o email do usuário")
+    print("\nCadastro de Usuário")
+    nome = input("Insira o nome do usuário: ")
+    email = input("Insira o email do usuário: ")
     
     while True:
         try:
@@ -56,13 +57,38 @@ def cadastrar_usuario(usuarios):
 
 def listar_usuario(usuarios):
     #Funçãpo para retornar a lista com os usuários cadastrados.
-    print("Lista de Usuários Cadastrados")
+    print("\nLista de Usuários Cadastrados")
     if not usuarios:
         print("Não existem usuários cadastrados")
         return
 
-    for i, usuario in enumerate(usuarios):
+    for i, usuario in enumerate(usuarios):  #Função utilizada para enumerar os objetos retornados pelo 'for', trazendo uma melhor legibilidade para o usuário
         print(f"Usuário #{i+1}")
-        print(f"Nome: {usuario[nome]}")
-        print(f"Email: {usuario[email]}")
-        print(f"Idade: {usuario[idade]}")
+        print(f"Nome: {usuario["nome"]}")
+        print(f"Email: {usuario["email"]}")
+        print(f"Idade: {usuario["idade"]}")
+
+def buscar_usuario(usuarios):
+    #Função para buscar o usuário pelo nome, na lista.
+    #Se encontrado, retornar seus dados cadastrados
+    print("\nBusca de Usuários")
+    if not usuarios:
+        print("Não existem usuários cadastrados")
+        return
+    
+    nome_busca = input("Digite o nome do usuário: ")
+    encontrado = False  #variável utilizada como indicador de estado, para tratar as ações em casos de encontrar ou não o nome desejado.
+    for usuario in usuarios:
+        if usuario["nome"].lower() == nome_busca.lower():  #Utilizado função 'lower' para tornar a busca case-insensitive
+            print("\nUsuário encontrado:")
+            print(f"Nome: {usuario["nome"]}")
+            print(f"Email: {usuario["email"]}")
+            print(f"Idade: {usuario["idade"]}")
+            encontrado = True
+
+    if not encontrado:
+        print(f"Usuário {nome_busca} não encontrado.")
+
+if __name__ == "__main__":   #Bloco utilizado para determinar que a função menu continue sendo excutada
+    menu()    
+
